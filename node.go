50d0374ce8f348673292d44f2b608b64fb16cdad
Ariel <ariel.har17@me.com>
2019-03-31 19:24:35 +0200

    feat(node): node parsing complete

--- node.go
+++ node.go
@@ -10,7 +10,7 @@ import (
 
 // Node of Gossip system
 type Node struct {
-	uiPort     int
+	UIPort     int
 	ip         string
 	gossipPort int
 	name       string
@@ -18,39 +18,59 @@ type Node struct {
 }
 
 func (node *Node) String() string {
-	return fmt.Sprintf("Node %s:\nUIPort: %s\n", node.name, node.ip)
+	return fmt.Sprintf("Node %s:\nUI Port: %s\nGossip Port: %d\nUI Port: %d\nPeers: [%s]",
+		node.name, node.ip, node.gossipPort, node.UIPort, node.PeersString())
 }
 
-func (node *Node) UIIPPort() string {
-	if len(node.ip) == 0 || node.uiPort <= 5000 {
-		return ""
+func (node *Node) PeersString() string {
+	var peersStr string
+	for _, node := range node.peers {
+		peersStr += node.GossipPortString() + ", "
 	}
+	return peersStr
+}
 
-	return fmt.Sprintf("%s:%d", node.ip, node.uiPort)
+func (node *Node) GossipPortString() string {
+	return fmt.Sprintf("%s:%d", node.ip, node.gossipPort)
 }
 
 func getNode(name *string, uiPort *int, gossipPort,
-	peers *string) (*Node, error) {
+	peersStr *string) (*Node, error) {
 	var node Node
 
 	ip, port, err := getIPPort(*gossipPort)
 	if err != nil {
 		return nil, err
 	}
-	node.name = *name
+
 	node.ip = ip
-	node.uiPort = *uiPort
 	node.gossipPort = port
+
+	if name == nil {
+		node.name = ""
+	} else {
+		node.name = *name
+	}
+
+	if uiPort == nil {
+		node.UIPort = 5001
+	} else {
+		node.UIPort = *uiPort
+	}
+
+	if peersStr != nil {
+		peers, err := getPeers(*peersStr)
+		if err != nil {
+			return nil, err
+		}
+		node.peers = peers
+	}
 
 	return node.Validate()
 }
 
 func (node *Node) Validate() (*Node, error) {
-	if len(node.name) == 0 {
-		return nil, errors.New("Node should have a name")
-	}
-
-	if node.gossipPort <= 5000 || node.uiPort <= 5000 {
+	if node.gossipPort <= 5000 || node.UIPort <= 5000 {
 		return nil, errors.New("Node ports should be higher than 5000")
 	}
 
@@ -69,6 +89,15 @@ func getIPPort(s string) (string, int, error) {
 	return info[0], port, err
 }
 
-func getPeers(s string) {
-
+func getPeers(s string) ([]*Node, error) {
+	nodeStrs := strings.Split(s, "_")
+	var nodes []*Node
+	for _, nodeStr := range nodeStrs {
+		node, err := getNode(nil, nil, &nodeStr, nil)
+		if err != nil {
+			return nil, err
+		}
+		nodes = append(nodes, node)
+	}
+	return nodes, nil
 }
